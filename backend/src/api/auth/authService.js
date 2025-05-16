1d3864bbcd20389b4ae7213ba7bbc112559ea874
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-16 11:38:56 -0300

    feat: add userRouter

--- backend/src/api/auth/authService.js
+++ backend/src/api/auth/authService.js
@@ -1,4 +1,4 @@
-const User = require('../user/user')
+const User = require('../user/userRouter')
 const express = require('express')
 
 module.exports = function (router) {
---
+++ backend/src/api/user/userRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao encontrar usuários' });
+  }
+});
+
+module.exports = router;
